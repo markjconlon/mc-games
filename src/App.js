6006c18869248a19aac0858d76e9ba9159c4ce46
markjconlon <markjconlon@gmail.com>
2017-10-03 15:57:27 -0400

    added Battleship.js module and css for displaying the players board

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Battleship from './Battleship'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Battleship />
+    );
+  }
+}
+
+export default App;
---
+++ src/Battleship.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+class Battleship extends Component {;
+  constructor(props) {
+    super(props);
+    this.state= {
+      playerTurn: 0,
+      playerShips: {
+        aircraftCarrier: { length: 5, symbol: 'L' },
+        battleship: { length: 4, symbol: 'M' },
+        submarine: { length: 3, symbol: 'N' },
+        cruiser: { length: 3, symbol: 'O' },
+        destroyer: { length: 2, symbol: 'P' }
+      },
+      playerBoard: [
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', ''
+      ],
+      computerBoard: [
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', '',
+        '', '', '', '', '', '', '', '', '', ''
+      ]
+    }
+  }
+  render(){
+    return(
+      <div className="playersView">
+        <div className="playerBoard">
+          {this.state.playerBoard.map((cell, index) => {
+            return <div className="cells" data-cell-id={index}>{cell}</div>;
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+export default Battleship;
