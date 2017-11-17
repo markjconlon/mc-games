be31043f17bb3ff21a5613d097bf828b0a92b395
markjconlon <markjconlon@gmail.com>
2017-11-16 19:59:21 -0500

    filter potential moves by computerMoves already made to make a list of potential moves; as an aside you cant compare arrays to arrays like in ruby you have to compare each element of the array

--- src/Battleship.js
+++ src/Battleship.js
@@ -150,13 +150,20 @@ class Battleship extends Component {
 
   computerFire() {
     const playerBoard = [...this.state.playerBoard]
-    const moves = [...this.state.computerMoves]
+    let moves = [...this.state.computerMoves]
     const potentialMoves= [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9], [1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7], [1, 8], [1, 9], [2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7], [2, 8], [2, 9], [3, 0], [3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [3, 6], [3, 7], [3, 8], [3, 9], [4, 0], [4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6], [4, 7], [4, 8], [4, 9], [5, 0], [5, 1], [5, 2], [5, 3], [5, 4], [5, 5], [5, 6], [5, 7], [5, 8], [5, 9], [6, 0], [6, 1], [6, 2], [6, 3], [6, 4], [6, 5], [6, 6], [6, 7], [6, 8], [6, 9], [7, 0], [7, 1], [7, 2], [7, 3], [7, 4], [7, 5], [7, 6], [7, 7], [7, 8], [7, 9], [8, 0], [8, 1], [8, 2], [8, 3], [8, 4], [8, 5], [8, 6], [8, 7], [8, 8], [8, 9], [9, 0], [9, 1], [9, 2], [9, 3], [9, 4], [9, 5], [9, 6], [9, 7], [9, 8], [9, 9]]
-    console.table(playerBoard);
-    console.log(potentialMoves);
     // takes the array of potentialMoves and subtracts all moves already made (which are stored in state)
-    const possibleMoves = potentialMoves.filter(function(move){
-      return !(moves.some(oldMove => oldMove === move));
+    let possibleMoves = potentialMoves.filter(function(move){
+      return !(moves.some(function(oldMove){
+        return oldMove[0] === move[0] && oldMove[1] === move[1];
+      }))
+    })
+    const move = possibleMoves[Math.floor(Math.random()* (possibleMoves.length))];
+    playerBoard[move[0]][move[1]] = playerBoard[move[0]][move[1]] + "f";
+    moves.push(move);
+    this.setState({
+      playerBoard: playerBoard,
+      computerMoves: moves
     })
   }
 
