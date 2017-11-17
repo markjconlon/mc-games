5e7622fd70526004aee4d6b52fb903a74cc78684
markjconlon <markjconlon@gmail.com>
2017-11-16 20:05:08 -0500

    logic to check if it was a hit and if it was to decrease playerHealth when the computer makes a hit in computerFire function

--- src/Battleship.js
+++ src/Battleship.js
@@ -126,7 +126,7 @@ class Battleship extends Component {
       const row = e.currentTarget.getAttribute("data-row-id")
       const cell = e.target.getAttribute("data-cell-id")
       const board = this.state.computerBoard.slice();
-      let computerHealth = this.state.computerHealth
+      let computerHealth = this.state.computerHealth;
       if ((this.state.computerBoard[row][cell]).split("").some(char => char === "f")) {
         alert("You have already fired here.")
       } else if(this.state.computerBoard[row][cell] === ''){
@@ -150,6 +150,7 @@ class Battleship extends Component {
 
   computerFire() {
     const playerBoard = [...this.state.playerBoard]
+    let playerHealth = this.state.playerHealth;
     let moves = [...this.state.computerMoves]
     const potentialMoves= [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9], [1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7], [1, 8], [1, 9], [2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7], [2, 8], [2, 9], [3, 0], [3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [3, 6], [3, 7], [3, 8], [3, 9], [4, 0], [4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6], [4, 7], [4, 8], [4, 9], [5, 0], [5, 1], [5, 2], [5, 3], [5, 4], [5, 5], [5, 6], [5, 7], [5, 8], [5, 9], [6, 0], [6, 1], [6, 2], [6, 3], [6, 4], [6, 5], [6, 6], [6, 7], [6, 8], [6, 9], [7, 0], [7, 1], [7, 2], [7, 3], [7, 4], [7, 5], [7, 6], [7, 7], [7, 8], [7, 9], [8, 0], [8, 1], [8, 2], [8, 3], [8, 4], [8, 5], [8, 6], [8, 7], [8, 8], [8, 9], [9, 0], [9, 1], [9, 2], [9, 3], [9, 4], [9, 5], [9, 6], [9, 7], [9, 8], [9, 9]]
     // takes the array of potentialMoves and subtracts all moves already made (which are stored in state)
@@ -159,6 +160,13 @@ class Battleship extends Component {
       }))
     })
     const move = possibleMoves[Math.floor(Math.random()* (possibleMoves.length))];
+
+    if (this.state.playerBoard[move[0]][move[1]] !== '') {
+      playerHealth -= 1;
+      this.setState({
+        playerHealth: playerHealth
+      })
+    }
     playerBoard[move[0]][move[1]] = playerBoard[move[0]][move[1]] + "f";
     moves.push(move);
     this.setState({
